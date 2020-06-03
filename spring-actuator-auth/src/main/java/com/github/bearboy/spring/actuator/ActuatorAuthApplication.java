163695c3b113bfa7e623cae4956af51fde370d63
bearBoy80 <jt.gui@szpcg.com>
2020-06-03 17:24:50 +0800

    优化无用的引用

--- spring-actuator-auth/src/main/java/com/github/bearboy/spring/actuator/ActuatorAuthApplication.java
+++ spring-actuator-auth/src/main/java/com/github/bearboy/spring/actuator/ActuatorAuthApplication.java
@@ -1,7 +1,6 @@
 package com.github.bearboy.spring.actuator;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.SpringVersion;
