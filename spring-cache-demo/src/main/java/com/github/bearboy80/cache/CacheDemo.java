0dfec0742e82455697f53d088951ac302c05a6d4
bearboy80 <2373319771@qq.com>
2022-08-19 18:47:19 +0800

    优化代码

--- spring-cache-demo/src/main/java/com/github/bearboy80/cache/CacheDemo.java
+++ spring-cache-demo/src/main/java/com/github/bearboy80/cache/CacheDemo.java
@@ -2,13 +2,7 @@ package com.github.bearboy80.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableCaching
