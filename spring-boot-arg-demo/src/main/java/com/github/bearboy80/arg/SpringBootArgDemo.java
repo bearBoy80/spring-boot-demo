cde363c15a27acf73719a851142d885a64302651
bearboy80 <2373319771@qq.com>
2022-08-23 15:18:22 +0800

    增加PropertySource优先级问题

--- spring-boot-arg-demo/src/main/java/com/github/bearboy80/arg/SpringBootArgDemo.java
+++ spring-boot-arg-demo/src/main/java/com/github/bearboy80/arg/SpringBootArgDemo.java
@@ -1,5 +1,6 @@
 package com.github.bearboy80.arg;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,7 @@ import java.util.List;
 
 /**
  * @author bearboy80
+ * application.yml 设置的端口9000将会被忽略，实现端口是8081
  */
 @SpringBootApplication
 public class SpringBootArgDemo {
@@ -19,6 +21,7 @@ public class SpringBootArgDemo {
         Collections.addAll(list, args);
         list.add("--debug=logfile.txt");
         list.add("--server.port=8081");
+        list.add("--userName=bearboy80");
         SpringApplication.run(SpringBootArgDemo.class, list.toArray(new String[list.size()]));
     }
 }
@@ -28,10 +31,10 @@ public class SpringBootArgDemo {
  */
 @Component
 class ArgsBean {
-    public ArgsBean(ApplicationArguments args) {
+    public ArgsBean(ApplicationArguments args, @Value("${userName}") String userName) {
+        System.out.println("接收到userName：" + userName);
         for (String key : args.getOptionNames()) {
             System.out.println(args.getOptionValues(key));
         }
-
     }
 }
