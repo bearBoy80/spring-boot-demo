470bf10c7df244fdd8122ca969653e155f059a24
bearboy80 <2373319771@qq.com>
2022-08-29 16:33:23 +0800

    增加spring boot task demo

---
+++ spring-boot-task-demo/readme.md
@@ -0,0 +1,7 @@
+# Spring Task
+## Spring Task 关键类
+ - TaskDecorator
+ - TaskExecutor  SyncTaskExecutor、SimpleAsyncTaskExecutor、ConcurrentTaskExecutor、ThreadPoolTaskExecutor、WorkManagerTaskExecutor、DefaultManagedTaskExecutor
+ - TaskScheduler 
+ - Trigger 
+ - TriggerContext 
---
+++ spring-boot-task-demo/src/main/java/com/github/bearboy/task/SpringBootTaskDemo.java
@@ -0,0 +1,14 @@
+package com.github.bearboy.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bearboy80
+ */
+@SpringBootApplication
+public class SpringBootTaskDemo {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTaskDemo.class, args);
+    }
+}
